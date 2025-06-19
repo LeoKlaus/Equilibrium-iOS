7442eaf95f59162898f661af046039695521e8e5
Leo Wehrfritz <leocommander.cs@gmail.com>
2025-06-19 14:45:49 +0200

    Initial Commit

---
+++ Equilibrium/EquilibriumApp.swift
@@ -0,0 +1,17 @@
+//
+//  EquilibriumApp.swift
+//  Equilibrium
+//
+//  Created by Leo Wehrfritz on 19.06.25.
+//
+
+import SwiftUI
+
+@main
+struct EquilibriumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
